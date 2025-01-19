f37ef374f42d7695677fe56d51ac3237b330c00f
Moon <82779002+Ramambo@users.noreply.github.com>
2025-01-19 16:07:06 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.08 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,19 +1,10 @@
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        int length = my_string.length();
-        String[] suffixes = new String[length];
-        
-        //모든 접미사를 생성하여 배열에 저장
-        for(int i=0; i<length; i++){
-            suffixes[i] = my_string.substring(i);
+        // String의 endsWith 메서드를 사용해 접미사인지 확인
+        if (my_string.endsWith(is_suffix)) {
+            return 1;
+        } else {
+            return 0;
         }
-// 접미사 배열에서 확인
-        for (int i = 0; i < length; i++) {
-            if (is_suffix.equals(suffixes[i])) {
-                return 1;
-            }
-        }
-        
-        return 0; // 접미사를 찾지 못한 경우
     }
 }
