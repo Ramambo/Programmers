5dc8d3c9a3fc5111d68688da254162c7308f6246
Moon <82779002+Ramambo@users.noreply.github.com>
2025-01-24 00:29:26 +0900

    [level 0] Title: 문자열 뒤집기, Time: 13.77 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,16 +1,18 @@
 class Solution {
     public String solution(String my_string, int s, int e) {
-        // 문자열의 앞부분 (s 이전까지)
+        //문자열의 앞부분 (s 이전까지)
         String prefix = my_string.substring(0,s);
-
-        // s부터 e까지의 부분 문자열 추출 및 뒤집기
-        String middle = my_string.substring(s, e + 1);
-        String reversedMiddle = new StringBuilder(middle).reverse().toString();
-
-        // 문자열의 뒷부분 (e 이후부터)
-        String suffix = my_string.substring(e + 1);
-
-        // 결과 문자열 조합
-        return prefix + reversedMiddle + suffix;
+        
+        //s부터 e까지의 부분 문자열 추출 및 뒤집기
+        String middle = my_string.substring(s,e+1);
+        String reverseMiddle = new StringBuilder(middle).reverse().toString();
+        
+        //문자열의 뒷부분(e이후부터)
+        String suffix = my_string.substring(e+1);
+        
+        //결과 문자열 조합
+        return prefix + reverseMiddle + suffix;
+        
+       
     }
 }
