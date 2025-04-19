4c5353bc7a09a4ee03b840a674f8bcf3f9e01a1d
Moon <82779002+Ramambo@users.noreply.github.com>
2025-04-19 22:27:26 +0900

    [Bronze I] Title: 단어 공부, Time: 468 ms, Memory: 32560 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        
+        word = word.toLowerCase();
+        int[] freq = new int[26];
+        
+        
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                freq[c - 'a']++;
+            }
+        }
+
+        // 가장 많이 나온 문자 찾기
+        int maxCount = 0;
+        char maxChar = '?';
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > maxCount) {
+                maxCount = freq[i];
+                maxChar = (char) (i + 'a');
+            } else if (freq[i] == maxCount) {
+                maxChar = '?';
+            }
+        }
+        
+        System.out.println(Character.toUpperCase(maxChar));
+    }
+}
