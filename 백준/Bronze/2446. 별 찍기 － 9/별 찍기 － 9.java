abd8af8f3777bb17595e56d481e65e7a600977f5
Moon <82779002+Ramambo@users.noreply.github.com>
2025-06-14 22:42:34 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 432 ms, Memory: 22332 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        // 위쪽별 (N줄)
+        for (int i = 0; i < N; i++) {
+            // 공백 출력
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            // 별 출력
+            for (int j = 0; j < 2 * (N - i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // 아래쪽 별 (N-1줄)
+        for (int i = 1; i < N; i++) {
+            // 공백 출력
+            for (int j = 0; j < N - i - 1; j++) {
+                System.out.print(" ");
+            }
+            // 별 출력
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
