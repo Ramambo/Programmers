1dcd131e3307ff9e527daaa094a8212bc571a0d0
Moon <82779002+Ramambo@users.noreply.github.com>
2025-06-30 18:08:25 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 196 ms, Memory: 18304 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int L = sc.nextInt();
+        int P = sc.nextInt();
+        int people = L * P;
+
+        for (int i = 0; i < 5; i++) {
+            int reported = sc.nextInt();
+            System.out.print((reported - people) + " ");
+        }
+    }
+}
