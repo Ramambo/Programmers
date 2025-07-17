39153f43d0f5145998577c41441a87b4b758f485
Moon <82779002+Ramambo@users.noreply.github.com>
2025-07-17 23:54:42 +0900

    [Bronze IV] Title: 평균 점수, Time: 168 ms, Memory: 17552 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        
+        for(int i=0; i<5; i++){
+            int score = sc.nextInt();
+            if(score < 40){
+                sum += 40;
+            }else{
+                sum += score;
+            }
+        }
+        
+        int avg = sum / 5;
+        
+        System.out.println(avg);
+    }
+}
